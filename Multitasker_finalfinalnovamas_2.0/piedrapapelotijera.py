e8618a0f54b7445c781af22bfada8bde8458387a
JDSimbaqueba <102933031+JDSimbaqueba@users.noreply.github.com>
2022-05-13 17:58:58 -0500

    Update piedrapapelotijera.py

--- Multitasker_finalfinalnovamas_2.0/piedrapapelotijera.py
+++ Multitasker_finalfinalnovamas_2.0/piedrapapelotijera.py
@@ -1,13 +1,13 @@
 import random
 import cv2
-
-
+##importa modulo random y cv2, que proviene de opencv
+##El modulo cv2 lo utilizamos para hacer aparecer una ventana emergente que tendra de imagen, la imagen dada. Al presionar cualquier tecla dicha ventana se cerrará.
 def si_jugador_gana(player, opponent):
     if (player == 'piedra' and opponent == 'tijera') or (player == 'tijera' and opponent == 'papel') or (
             player == 'papel' and opponent == 'piedra'):
         return True
     elif (player == opponent):
-        return "Tie"
+        return "EMPATE"
     else:
         return False
 def jugar(jugador):
@@ -40,7 +40,7 @@ def jugar(jugador):
         cv2.waitKey(3000)
         cv2.destroyAllWindows()
         return 'Has perdido...'
-    elif si_jugador_gana(jugador, ordenador) == "Tie":
+    elif si_jugador_gana(jugador, ordenador) == "EMPATE":
         piedra=cv2.imread("empate.png",1)
         cv2.imshow("EMPATE",piedra)
         cv2.waitKey(3000)
